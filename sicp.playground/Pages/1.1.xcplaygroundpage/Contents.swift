27ab97976383b710bacd95b52eb5b1e030fb3202
Matt Dupree <kmdupr33@gmail.com>
2016-04-12 17:37:10 -0400

    Initial commit

---
+++ sicp.playground/Pages/1.1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+/*:
+ # 1.1
+ 
+ 
+ 
+ */
+
+
+//: [Next](@next)
---
+++ sicp.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+/*:
+ # SICP in Swift
+ 
+ The [Structure and Interpretation of Computer Programs](https://mitpress.mit.edu/sicp/full-text/book/book.html) is widely regarded as essential reading for serious programmers. Unfortunately, the programming language used in SICP (viz., scheme) isn't one that's in serious use anywhere. Moreover, the tools avilable for working with scheme are quite limited.
+ 
+ This set of playgrounds is an attempt to provide an update for the tooling and language students of SICP need to use in order to grok SICP's concepts. SICP, after all, is not about a particular programming language:
+ 
+ >...we believe that the essential material to be addressed by a subject at this level is not the syntax of particular programming-language constructs, nor clever algorithms for computing particular functions efficiently, nor even the mathematical analysis of algorithms and the foundations of computing, but rather the techniques used to control the intellectual complexity of large software systems.
+ >
+ > \- *Abelson et. al.*, pg. 12
+ 
+ 
+ This set of playgrounds is also an attempt to summarize SICP. This goal is more self-serving than the previous one, as it is unlikely that my summary will retain all of insight and clarity of the original authors of the text. I hope, however, my attempt to summarize the sections of SICP will help solidify its concepts in my own mind, and if you happen to find the summary useful, then that's great. In my summaries, I have tried to include links to the relevant sections of the original text in case you find my summarizing unhelpful.
+ 
+ 
+ */
+
+
+//: [Next](@next)
