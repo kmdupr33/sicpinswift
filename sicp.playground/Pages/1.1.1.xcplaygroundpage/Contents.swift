3e080dcd8963f5684d18ac43898e10f18d0b546d
Matt Dupree <kmdupr33@gmail.com>
2016-04-13 10:55:34 -0400

    Add note about infix notation

--- sicp.playground/Pages/1.1.1.xcplaygroundpage/Contents.swift
+++ sicp.playground/Pages/1.1.1.xcplaygroundpage/Contents.swift
@@ -18,6 +18,7 @@
     add(1, 41)
 
 /*:
+ * note: Those who are familiar with swift may wonder why I don't use `1 + 41` instead of `add(1, 41)`. I'm doing this so that I can avoid having to talk about [infix notation](https://en.wikipedia.org/wiki/Infix_notation). Throughout this summary, I avoid any expressions that use infix notation.
  
  The `add` sign in the above string of characters is an "operand;" it operates on the operators, 1 and 41 that are enclosed between two parentheses. `add`, the operand, represents a *primitive* procedure. The `add` procedure adds `1` and `41` to produce the result you see on the right. Let's call a combination of operators and operands an "invocation." `1 + 41` above is an example of a *primitive* invocation.
  
