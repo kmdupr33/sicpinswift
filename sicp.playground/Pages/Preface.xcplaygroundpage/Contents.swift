a3fbaa75621431a6bffef2e8726c7038b08d81d4
Matt Dupree <kmdupr33@gmail.com>
2016-04-13 15:08:38 -0400

    Fix block quote in preface

--- sicp.playground/Pages/Preface.xcplaygroundpage/Contents.swift
+++ sicp.playground/Pages/Preface.xcplaygroundpage/Contents.swift
@@ -7,9 +7,10 @@
  
  * callout( ):...we believe that the essential material to be addressed by a subject at this level is not the syntax of particular programming-language constructs, nor clever algorithms for computing particular functions efficiently, nor even the mathematical analysis of algorithms and the foundations of computing, but rather the techniques used to control the intellectual complexity of large software systems.
  
+    \- *Abelson et. al.*, pg. 12
+ 
  This set of playgrounds is an attempt to provide an update for the tooling and language students of SICP need to use in order to grok SICP's concepts. More specifically, it gives students a straight foward way to test whether their answers to the exercises are correct and it gives them the opportunity to learn a language that will likely be more useful to them as professional programmers.
- >
- > \- *Abelson et. al.*, pg. 12
+ 
  
  
  This set of playgrounds is also an attempt to summarize SICP. This goal is more self-serving than the previous one, as it is unlikely that my summary will retain all of insight and clarity of the the original text. I hope, however, my attempt to summarize the sections of SICP will help solidify its concepts in my own mind, and if you happen to find the summary useful, then that's great. In my summaries, I have tried to include references to the relevant sections of the original text in case you find my summarizing unhelpful. The page numbers mentioned in these references are the pages of [the pdf version of SICP](http://web.mit.edu/alexmv/6.037/sicp.pdf).
